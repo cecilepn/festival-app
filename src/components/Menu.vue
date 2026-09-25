eb902a7161d5a87428c6b27cc745010ab357c024
Cécile <cecilephan@outlook.com>
2026-09-25 14:25:25 +0200

    fix(header): fix style

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -35,12 +35,12 @@
     <nav
       v-if="menu"
       aria-label="Navigation principale"
-      class="menu flex gap-32 justify-between">
+      class="menu flex gap-32 justify-between items-center">
       <a href="/">
         <img src="/logo.png" alt="" />
       </a>
       <div
-        class="menu__links flex items-center w-fit body-s-semi-bold gap-48 justify-center">
+        class="menu__links flex items-center w-fit body-m-semi-bold gap-48 justify-center">
         <template
           v-for="(item, index) in navigationItems"
           :key="item.key ?? index">
@@ -72,6 +72,6 @@
   .menu__links {
     border: solid 2px var(--color-black);
     border-radius: var(--spacing-32);
-    padding: 0px var(--spacing-64);
+    padding: var(--spacing-16) var(--spacing-64);
   }
 </style>
